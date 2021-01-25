51857371ecdb57c06e191b34926867edd0d21d95
Bilaboz <44091093+Bilaboz@users.noreply.github.com>
2021-01-26 00:03:04 +0100

    Code cleanup + good practices
    Hey, I switched your functions to arrow functions, did some cleanup and removed the usage of var
    Using arrow functions is a good practice you can read more here https://medium.com/better-programming/why-javascript-arrow-functions-are-useful-aa023bad403a
    And also you should not use var https://hackernoon.com/why-you-shouldnt-use-var-anymore-f109a58b9b70

--- index.js
+++ index.js
@@ -1,171 +1,162 @@
-const {PromiseSocket} = require("promise-socket");
+const { PromiseSocket } = require("promise-socket");
 const sha1 = require("js-sha1");
-const cluster = require('cluster');
+const cluster = require("cluster");
 const net = require("net");
-const https = require("https");
+const RL = require("readline");
 
-const RL = require('readline');
+let user = "",
+    processes = 1;
 
-var user = "",
-	processes = 1;
-	
 console.clear();
 
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if(!args[0]) return console.log("Error, please run [node index.js username threads]");
-if(!args[1]) return console.log("Error, please run [node index.js username threads]");
+if (!args[0]) return console.log("Error, please run [node index.js username threads]");
+if (!args[1]) return console.log("Error, please run [node index.js username threads]");
 
 user = args[0];
 processes = args[1];
-console.log("Miner Started for user ("+user+") with "+processes+" threads");
+console.log("Miner Started for user (" + user + ") with " + processes + " threads");
 
+const calculateHashrate = (hashes) => {
+    hashes = parseFloat(hashes);
+    let hashrate = hashes.toFixed(2) + " h/s";
 
-if (cluster.isMaster) {
-	
-   var threads = [];
+    if (hashes / 1000 > 0.5) hashrate = (hashes / 1000).toFixed(2) + " Kh/s";
+    if (hashes / 1000000 > 0.5) hashrate = (hashes / 1000000).toFixed(2) + " Mh/s";
+    if (hashes / 1000000000 > 0.5) hashrate = (hashes / 1000000000).toFixed(2) + " Gh/s";
 
-  for (let i = 0; i < processes; i++) {
-	var worker = cluster.fork();
-	
-	var data = {}
-	data.hashes = 0;
-	data.rejected = 0;
-	data.accepted = 0;
-	
-	threads.push(data)
-	
-	worker.on('message', function(msg) {
-		threads[msg.workerId].hashes = msg.hashes;
-		threads[msg.workerId].rejected = msg.rejected;
-		threads[msg.workerId].accepted = msg.accepted;
-		printData();
-    });
-  }
-  
-  function calculateHashrate(hashes)
-  {
-	hashes = parseFloat(hashes);
-	var hashrate = hashes.toFixed(2) + " h/s"
-	
-	if((hashes / 1000) > 0.5) hashrate = (hashes / 1000).toFixed(2) + " Kh/s";
-	if((hashes / 1000000) > 0.5) hashrate = (hashes / 1000000).toFixed(2) + " Mh/s";
-	if((hashes / 1000000000) > 0.5) hashrate = (hashes / 1000000000).toFixed(2) + " Gh/s";
-	
-	return hashrate;
-  }
-  
-  function printData() {
+    return hashrate;
+};
+
+const printData = (threads) => {
     RL.cursorTo(process.stdout, 0, 0);
     RL.clearLine(process.stdout, 0);
     RL.clearScreenDown(process.stdout);
 
-	  var rows = [];
-	  for (const i in threads) {
-		rows.push({
-			'Hashrate': calculateHashrate(threads[i].hashes),
-			'Accepted': threads[i].accepted,
-			'Rejected': threads[i].rejected
-		});
-	  }
-	  
-	  var hr = 0, acc = 0, rej = 0;
-	  
-	  for (const i in threads) {
-		hr = hr + threads[i].hashes;
-		acc = acc + threads[i].accepted;
-		rej = rej + threads[i].rejected;
-	  }
-	  
-		rows["Total"] = {
-			'Hashrate': calculateHashrate(hr),
-			'Accepted': acc,
-			'Rejected': rej,
-		};
-		
-	  console.table(rows);
-	  rows = [];
-	}
-  
-} else if (cluster.isWorker) {
+    let rows = [];
+    for (const i in threads) {
+        rows.push({
+            Hashrate: calculateHashrate(threads[i].hashes),
+            Accepted: threads[i].accepted,
+            Rejected: threads[i].rejected,
+        });
+    }
 
-	var data = {}
-	data.workerId = cluster.worker.id-1;
-	data.hashes = 0;
-	data.rejected = 0;
-	data.accepted = 0;
-	
-	let socket = new net.Socket();
-	const promiseSocket = new PromiseSocket(socket);
+    let hr = 0,
+        acc = 0,
+        rej = 0;
 
-	socket.setEncoding('utf8');
-	socket.connect(2811, "51.15.127.80");
+    for (const i in threads) {
+        hr = hr + threads[i].hashes;
+        acc = acc + threads[i].accepted;
+        rej = rej + threads[i].rejected;
+    }
 
-	function findNumber(prev, toFind, diff) {
-		return new Promise((resolve, reject) => {
-			for (let i = 0; i < 100 * diff; i++) {
-				let hash = sha1(prev + i);
-				
-				data.hashes = data.hashes + 1;
-				
-				if (hash == toFind) {
-					//console.log("Hash Found!");
-					
-					socket.write(i.toString() + ",,Node.JS Miner v2.0");
-					resolve();
-					break;
-				}
-			}
-		})
-	}
+    rows["Total"] = {
+        Hashrate: calculateHashrate(hr),
+        Accepted: acc,
+        Rejected: rej,
+    };
 
-	async function startMining() { // start the mining process
-		while (true) {
-			socket.write("JOB,${user}");
-			let job = await promiseSocket.read();
-			job = job.split(",");
+    console.table(rows);
+    rows = [];
+};
 
-			let prev = job[0];
-			let toFind = job[1];
-			let diff = job[2];
+const findNumber = (prev, toFind, diff) => {
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < 100 * diff; i++) {
+            let hash = sha1(prev + i);
 
-			// console.log("got job! difficulty: " + diff);
+            data.hashes = data.hashes + 1;
 
-			await findNumber(prev, toFind, diff);
-			var str = await promiseSocket.read();
-			if(str == "GOOD")
-			{
-				data.accepted = data.accepted + 1;
-				//console.log("Good Job!")
-			}
-			else {
-				data.rejected = data.rejected + 1;
-				//console.log(str);
-			}
-			sendData();
-			data.hashes = 0;
-		}
-	}
-	
-	function sendData() {
-		process.send(data);
-	}
+            if (hash == toFind) {
+                socket.write(i.toString() + ",,Node.JS Miner v2.0");
+                resolve();
+                break;
+            }
+        }
+    });
+};
 
-	socket.once("data", (data) => {	// login process
-		if (data.includes("1.9")) {
-			startMining();
-		} else {
-			console.log(data.slice(3));
-			socket.end();
-		}
-	});
+const startMining = async () => {
+    // start the mining process
+    while (true) {
+        socket.write("JOB,${user}");
+        let job = await promiseSocket.read();
+        job = job.split(",");
 
-	socket.on("end", () => {
-		console.log("Connection ended");
-	})
+        const prev = job[0];
+        const toFind = job[1];
+        const diff = job[2];
 
-	socket.on("error", (err) => {
-		console.log(`Socket error: ${err}`);
-	})
-	
+        await findNumber(prev, toFind, diff);
+        const str = await promiseSocket.read();
+        if (str == "GOOD") {
+            data.accepted = data.accepted + 1;
+        } else {
+            data.rejected = data.rejected + 1;
+        }
+        sendData();
+        data.hashes = 0;
+    }
+};
+
+const sendData = () => {
+    process.send(data);
+};
+
+if (cluster.isMaster) {
+    let threads = [];
+
+    for (let i = 0; i < processes; i++) {
+        let worker = cluster.fork();
+
+        let data = {};
+        data.hashes = 0;
+        data.rejected = 0;
+        data.accepted = 0;
+
+        threads.push(data);
+
+        worker.on("message", (msg) => {
+            threads[msg.workerId].hashes = msg.hashes;
+            threads[msg.workerId].rejected = msg.rejected;
+            threads[msg.workerId].accepted = msg.accepted;
+            printData(threads);
+        });
+    }
+    return;
 }
+
+// I removed the else statement to put vairables in the global scope
+
+let data = {};
+data.workerId = cluster.worker.id - 1;
+data.hashes = 0;
+data.rejected = 0;
+data.accepted = 0;
+
+let socket = new net.Socket();
+const promiseSocket = new PromiseSocket(socket);
+
+socket.setEncoding("utf8");
+socket.connect(2811, "51.15.127.80");
+
+socket.once("data", (data) => {
+    // login process
+    if (data.includes("1.9")) {
+        startMining();
+    } else {
+        console.log(data.slice(3));
+        socket.end();
+    }
+});
+
+socket.on("end", () => {
+    console.log("Connection ended");
+});
+
+socket.on("error", (err) => {
+    console.log(`Socket error: ${err}`);
+});
